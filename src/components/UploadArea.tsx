9cbc41a867a5cf61a46cb12a6f13612c2dc670ce
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-09-28 19:07:57 +0800

    fix: click camera input, not the btn

--- src/components/UploadArea.tsx
+++ src/components/UploadArea.tsx
@@ -35,7 +35,7 @@ export default function UploadArea({
 
   const handleCameraBtnClicked = () => {
     if (isWorking) return;
-    cameraBtnRef.current?.click();
+    cameraInputRef.current?.click();
   };
 
   // Add keybindings
