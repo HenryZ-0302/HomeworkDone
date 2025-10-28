39ef31d94f7d9e1bb8c8311f8da8d535ccd9306e
HenryZ-0302 <z6798581@gmail.com>
2025-10-28 15:49:07 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// ⬇️ 新增这一行
+import PasswordGate from "./components/PasswordGate";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* ⬇️ 用密码门包起来 */}
+    <PasswordGate>
+      <App />
+    </PasswordGate>
   </StrictMode>,
 );
